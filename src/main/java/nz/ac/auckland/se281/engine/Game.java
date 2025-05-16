336584ad6e656dec989abc13f16e6195eedc2c99
Jim Zhou <jzho655@aucklanduni.ac.nz>
2025-05-16 21:06:34 +1200

    Passed task 1 test cases 2-4, can keep track of rounds

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -5,6 +5,9 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
+  int totalRounds;
+  int currentRound;
+
 
 
   public Game() {}
@@ -12,10 +15,19 @@ public class Game {
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
     String namePlayer = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+    this.totalRounds = numRounds;
+    this.currentRound = 0;
 
   }
 
   public void play() {
+    if (currentRound < totalRounds) {
+      currentRound++;
+      MessageCli.START_ROUND.printMessage(currentRound, totalRounds);
+    } else {
+      MessageCli.PRINT_END_GAME.printMessage();
+    }
+
   }
 
   public void showStats() {}
