a0ff8611f08204dc5fd0ef6fd108bedbb9024ffc
Jim Zhou <jzho655@aucklanduni.ac.nz>
2025-05-16 21:01:00 +1200

    Passed task 1 test case 1 and added .idea/ to .gitignore

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -1,15 +1,22 @@
 package nz.ac.auckland.se281.engine;
+import nz.ac.auckland.se281.cli.MessageCli;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
 
+
   public Game() {}
 
-  public void newGame(Difficulty difficulty, int numRounds, String[] options) {}
+  public void newGame(Difficulty difficulty, int numRounds, String[] options) {
+    String namePlayer = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
 
-  public void play() {}
+  }
+
+  public void play() {
+  }
 
   public void showStats() {}
 }
