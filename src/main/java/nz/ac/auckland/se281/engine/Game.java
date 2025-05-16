3794c6f424f4df0975e215049e4dcdbda1b61d63
Jim Zhou <jzho655@aucklanduni.ac.nz>
2025-05-16 22:20:16 +1200

    Passed task 1 test cases 7-13 and 17-18, can now determine if inputs are correct

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,19 +2,22 @@ package nz.ac.auckland.se281.engine;
 import nz.ac.auckland.se281.cli.MessageCli;
 
 import nz.ac.auckland.se281.Main.Difficulty;
+import nz.ac.auckland.se281.cli.Utils;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
   int totalRounds;
   int currentRound;
+  public String playerName;
 
 
 
   public Game() {}
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
-    String namePlayer = options[0];
-    MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
+    this.playerName = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(this.playerName);
     this.totalRounds = numRounds;
     this.currentRound = 1;
 
@@ -28,8 +31,23 @@ public class Game {
     currentRound++;
 
     MessageCli.ASK_HUMAN_INPUT.printMessage();
+    String[] parts = Utils.scanner.nextLine().trim().split("\\s+");
+
+    while (parts.length != 2 ||
+            Colour.fromInput(parts[0]) == null ||
+            Colour.fromInput(parts[1]) == null) {
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+    parts = Utils.scanner.nextLine().trim().split("\\s+");
+    }
+    Colour ownColor = Colour.fromInput(parts[0]);
+    Colour guessColour = Colour.fromInput(parts[1]);
+    MessageCli.PRINT_INFO_MOVE.printMessage(this.playerName,ownColor, guessColour);
+    
+
+
 
   }
+
 
   public void showStats() {}
 }
