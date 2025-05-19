69dd6ca059d45720f5bc6b2a8dc4a0b4521b1054
Jim Zhou <jzho655@aucklanduni.ac.nz>
2025-05-20 00:23:30 +1200

    Passed task 2 test cases 7-10, correctly distributes earned points to ai and player

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -6,7 +6,6 @@ import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.Utils;
 import nz.ac.auckland.se281.engine.ai.Ai;
 import nz.ac.auckland.se281.engine.ai.AiFactory;
-import nz.ac.auckland.se281.engine.ai.EasyAi;
 import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
@@ -20,12 +19,14 @@ public class Game {
 
   public Game() {}
 
-  public void addScore(boolean powerColour, boolean isPlayer) {
-    int points;
-    if (powerColour) {
+  public int addScore(
+      Colour guessColour, Colour otherPlayersColour, Colour powerColour, boolean isPlayer) {
+    int points = 1;
+    if (guessColour != otherPlayersColour) {
+      return 0;
+    }
+    if (guessColour == powerColour) {
       points = 3;
-    } else {
-      points = 1;
     }
 
     if (isPlayer) {
@@ -33,6 +34,7 @@ public class Game {
     } else {
       aiScore += points;
     }
+    return points;
   }
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
@@ -59,9 +61,10 @@ public class Game {
       parts = Utils.scanner.nextLine().trim().split("\\s+");
     }
     boolean isPowerColourRnd = false;
+    Colour powerColour = null;
     if (currentRound % 3 == 0) {
       isPowerColourRnd = true;
-      Colour powerColour = Colour.getRandomColourForPowerColour();
+      powerColour = Colour.getRandomColourForPowerColour();
       MessageCli.PRINT_POWER_COLOUR.printMessage(powerColour);
     }
     Colour ownColor = Colour.fromInput(parts[0]);
@@ -69,14 +72,16 @@ public class Game {
     ai.chooseColour();
     ai.AiConfirmMessage();
     MessageCli.PRINT_INFO_MOVE.printMessage(this.playerName, ownColor, guessColour);
-    if (guessColour == ai.getOwnColour()) {
-      addScore(isPowerColourRnd, true);
-    }
-    if (ai.getGuessColour() == ownColor) {
-      addScore(isPowerColourRnd, false);
-    }
-    MessageCli.PRINT_OUTCOME_ROUND.printMessage(this.playerName, this.playerScore);
-    MessageCli.PRINT_OUTCOME_ROUND.printMessage(Game.AI_NAME, this.aiScore);
+
+    int playerEarnedPoints = 0;
+    int aiEarnedPoints = 0;
+
+    playerEarnedPoints = addScore(guessColour, ai.getOwnColour(), powerColour, true);
+
+    aiEarnedPoints = addScore(ai.getGuessColour(), ownColor, powerColour, false);
+
+    MessageCli.PRINT_OUTCOME_ROUND.printMessage(this.playerName, playerEarnedPoints);
+    MessageCli.PRINT_OUTCOME_ROUND.printMessage(Game.AI_NAME, aiEarnedPoints);
     currentRound++;
   }
 
